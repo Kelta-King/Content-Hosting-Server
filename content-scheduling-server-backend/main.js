6bff57b4ad642818ab54a642f65033b954cee783
Kelta-King <kushangshah131@gmail.com>
2023-05-28 18:14:07 +0530

    Provide Schedule API part

--- content-scheduling-server-backend/main.js
+++ content-scheduling-server-backend/main.js
@@ -9,6 +9,20 @@ const port = 8000;
 
 app.use(function (req, res, next) {
     res.setHeader("Content-Type", "application/json");
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
 });
 
@@ -64,6 +78,13 @@ app.post('/fileupload', (req, res) => {
     });
 });
 
+app.get("/provideSchedule", (req, res) => {
+    console.log(req.query);
+    var obj = {
+        "jey": "Value",
+    }
+    res.end(JSON.stringify(obj));
+});
 
 app.listen(port, () => {
     console.log("listening to the port: " + port);
